3cdfcd65ac47feafa351eed37da4bd0377e65f9b
ArjunSahlot <iarjun.sahlot@gmail.com>
2023-05-04 09:45:36 -0700

    web

--- server.js
+++ server.js
@@ -13,20 +13,56 @@ const config = new Configuration({
 })
 const openai = new OpenAIApi(config);
 
-app.post('/chatgpt', async (req, res) => {
+app.post('/pawanchatgpt', async (req, res) => {
   try {
-    const prompt = req.body.prompt;
+    const messages = req.body.messages;
 
     const response = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
       messages: [{ "role": "user", "content": prompt }],
-      temperature: 0.7,
+      temperature: req.body.temperature || 0.7,
     })
 
     const answer = response.data.choices[0].message.content;
     res.status(200).json({ answer });
   } catch (error) {
     console.error(JSON.stringify(error.response.data));
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post('/webchatgpt', async (req, res) => {
+  try {
+    const messages = req.body.messages;
+    const token = req.body.token;
+    const model = req.body.model;
+    const cookie = req.body.cookie;
+
+    const response = await fetch(`https://chat.openai.com/backend-api/conversation`, {
+      method: 'POST',
+      // mode: 'no-cors',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+        ...(cookie && { Cookie: cookie }),
+      },
+      body: JSON.stringify({
+        action: 'next',
+        // conversation_id: session.conversationId,
+        messages: messages,
+        model: model || 'text-davinci-002-render-sha',
+        timezone_offset_min: new Date().getTimezoneOffset(),
+        variant_purpose: 'none',
+        history_and_training_disabled: true,
+      })
+    });
+
+    console.log(response);
+    const answer = response.data.choices[0].message.content;
+    res.status(200).json({ answer });
+  } catch (error) {
+    console.log(error);
     res.status(500).json({ error: error.message });
   }
 });
